b056cd67831a784e6331bdd5218f6d4bc7ce1699
grzegorz103 <grzesiek555_12@outlook.com>
2019-12-03 18:19:23 +0100

    Add king movements

--- src/app/models/king.ts
+++ src/app/models/king.ts
@@ -15,7 +15,43 @@ export class King extends Piece {
         let row = this.point.row;
         let col = this.point.col;
 
-       
+        // lewo
+        if (BoardComponent.isFieldEmpty(row, col - 1)) {
+            possiblePoints.push(new Point(row, col - 1));
+        }
+
+        // prawo
+        if (BoardComponent.isFieldEmpty(row, col + 1)) {
+            possiblePoints.push(new Point(row, col + 1));
+        }
+
+        // dol
+        if (BoardComponent.isFieldEmpty(row + 1, col)) {
+            possiblePoints.push(new Point(row + 1, col));
+        }
+
+        // gora
+        if (BoardComponent.isFieldEmpty(row - 1, col)) {
+            possiblePoints.push(new Point(row - 1, col));
+        }
+
+        // lewo gora
+        if (BoardComponent.isFieldEmpty(row - 1, col - 1)) {
+            possiblePoints.push(new Point(row - 1, col - 1));
+        }
+        // prawo gora
+        if (BoardComponent.isFieldEmpty(row - 1, col + 1)) {
+            possiblePoints.push(new Point(row - 1, col + 1));
+        }
+
+        // lewo dol
+        if (BoardComponent.isFieldEmpty(row + 1, col - 1)) {
+            possiblePoints.push(new Point(row + 1, col - 1));
+        }
+        // prawo dol
+        if (BoardComponent.isFieldEmpty(row + 1, col + 1)) {
+            possiblePoints.push(new Point(row + 1, col + 1));
+        }
         return possiblePoints;
     }
 
