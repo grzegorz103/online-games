dff33ead6a52bbb7f00d1b5039449094a0513fe0
grzegorz103 <grzesiek555_12@outlook.com>
2019-12-03 17:50:17 +0100

    Fill bishop diagonal movements

--- src/app/models/bishop.ts
+++ src/app/models/bishop.ts
@@ -17,7 +17,30 @@ export class Bishop extends Piece {
 
         if (this.color === Color.WHITE) {
             for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) { // lewa gorna przekatna
-                console.log(BoardComponent.isFieldEmpty(i,col));
+                if (!BoardComponent.isFieldTakenByEnemy(i, j, Color.BLACK) && BoardComponent.isFieldEmpty(i, j)) {
+                    possiblePoints.push(new Point(i, j));
+                } else {
+                    break;
+                }
+            }
+
+            for (let i = row - 1, j = col + 1; i >= 0 && j < 8; --i, ++j) { // prawa gorna przekatna
+                if (!BoardComponent.isFieldTakenByEnemy(i, j, Color.BLACK) && BoardComponent.isFieldEmpty(i, j)) {
+                    possiblePoints.push(new Point(i, j));
+                } else {
+                    break;
+                }
+            }
+
+            for (let i = row + 1, j = col -1; i < 8 && j >= 0; ++i, --j) { // lewa dolna przekatna
+                if (!BoardComponent.isFieldTakenByEnemy(i, j, Color.BLACK) && BoardComponent.isFieldEmpty(i, j)) {
+                    possiblePoints.push(new Point(i, j));
+                } else {
+                    break;
+                }
+            }
+
+            for (let i = row + 1, j = col + 1; i < 8 && j < 8; ++i, ++j) { // prawa dolna przekatna
                 if (!BoardComponent.isFieldTakenByEnemy(i, j, Color.BLACK) && BoardComponent.isFieldEmpty(i, j)) {
                     possiblePoints.push(new Point(i, j));
                 } else {
