b1080de3eafd2a10771628c77a46e9cd3440dae2
Your Name <you@example.com>
2019-12-03 17:00:15 +0100

    Add knight model

--- src/app/models/knight.ts
+++ src/app/models/knight.ts
@@ -1,6 +1,13 @@
 import { Piece } from './piece';
+import { Point } from './point';
+import { Color } from './color';
 
 export class Knight extends Piece {
+
+    constructor(point: Point, color: Color, image: string) {
+        super(point, color, image);
+    }
+    
     getPossibleMoves(): import("./point").Point[] {
         throw new Error("Method not implemented.");
     }
