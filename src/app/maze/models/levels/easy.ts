e9db1425afbe57dce1082f5913bc3cb4447b2aff
grzegorz103 <grzesiek555_12@outlook.com>
2019-12-12 16:28:59 +0100

    Add hard level impl

---
+++ src/app/maze/models/levels/easy.ts
@@ -0,0 +1,8 @@
+import { Level } from './level';
+
+export class Easy extends Level{
+    move(computer: import("../computer").Computer): void {
+        throw new Error("Method not implemented.");
+    }
+    
+}
---
+++ src/app/maze/models/levels/hard.ts
@@ -0,0 +1,47 @@
+import { Level } from './level';
+import { Computer } from '../computer';
+import { MazeComponent } from '../../maze/maze.component';
+import { Point } from '../point';
+
+export class Hard extends Level {
+
+    move(computer: Computer): void {
+        if (computer) {
+            let neighbours = MazeComponent.neighbours(new Point(computer.row, computer.col, null));
+            let move = 0;
+
+            if (neighbours.some(e => e === computer.direction)) { // zmienic na if neighbours.length > 2 (gdy jest wiecej niz 2 mozliwe sciezzki)
+                //    let random = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+                if (neighbours.length > 2) {
+                    move = neighbours.filter(e => e !== MazeComponent.oppositeDirections[computer.direction])[Math.floor(Math.random() * (neighbours.length - 1))];
+                } else {
+                    move = computer.direction;
+                }
+            } else {
+                if (neighbours.length > 1) {
+                    // jezeli jest wiecej niz 1 sasiad to bierzemy lewego lub prawego
+                    move = neighbours.filter(e => e !== MazeComponent.oppositeDirections[computer.direction])[Math.floor(Math.random() * (neighbours.length - 1))];
+                } else {
+                    move = neighbours[Math.floor(Math.random() * neighbours.length)];
+                }
+            }
+            switch (move) {
+                case MazeComponent.UP:
+                    computer.row -= 1;
+                    break;
+                case MazeComponent.DOWN:
+                    computer.row += 1;
+                    break;
+                case MazeComponent.LEFT:
+                    computer.col -= 1;
+                    break;
+                case MazeComponent.RIGHT:
+                    computer.col += 1;
+                    break;
+            }
+
+            computer.direction = move;
+        }
+    }
+
+}
