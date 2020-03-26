663b5fbc0b4c3f3aee0be6a7ce06b804688cbaa4
grzegorz103 <grzesiek555_12@outlook.com>
2020-03-26 19:58:28 +0100

    Fix issue with moving right

--- src/main/java/chess/api/services/MazeServiceImpl.java
+++ src/main/java/chess/api/services/MazeServiceImpl.java
@@ -71,7 +71,7 @@ public class MazeServiceImpl {
                         player.getPoint().setCol(player.getPoint().getCol() - 1);
                     break;
                 case 4:
-                    if (playerPoint.getRow() < 28 && points[playerPoint.getRow()][playerPoint.getCol() + 1].isOccupied())
+                    if (playerPoint.getCol() < 28 && points[playerPoint.getRow()][playerPoint.getCol() + 1].isOccupied())
                         player.getPoint().setCol(player.getPoint().getCol() + 1);
                     break;
             }
