2260aaffe9f771643b9196b7bce84853a6af13dd
grzegorz103 <grzesiek555_12@outlook.com>
2020-03-26 18:23:32 +0100

    Update players movements

--- src/main/java/chess/api/services/MazeServiceImpl.java
+++ src/main/java/chess/api/services/MazeServiceImpl.java
@@ -51,20 +51,29 @@ public class MazeServiceImpl {
         Maze maze = games.get(uri);
 
         if (maze != null) {
-            Player player = maze.getPlayers().stream().filter(e -> Objects.equals(sessionId, e.getSessionId()))
-                    .findFirst().orElseThrow(() -> new RuntimeException("Not found"));
+            Player player = maze.getPlayers()
+                    .stream()
+                    .filter(e -> Objects.equals(sessionId, e.getSessionId()))
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Not found"));
+            Point[][] points = maze.getPoints();
+            Point playerPoint = player.getPoint();
             switch (move) {
                 case 1:
-                    player.getPoint().setRow(player.getPoint().getRow() - 1);
+                    if (playerPoint.getRow() > 0 && points[playerPoint.getRow() - 1][playerPoint.getCol()].isOccupied())
+                        player.getPoint().setRow(player.getPoint().getRow() - 1);
                     break;
                 case 2:
-                    player.getPoint().setRow(player.getPoint().getRow() + 1);
+                    if (playerPoint.getRow() < 28 && points[playerPoint.getRow() + 1][playerPoint.getCol()].isOccupied())
+                        player.getPoint().setRow(player.getPoint().getRow() + 1);
                     break;
                 case 3:
-                    player.getPoint().setCol(player.getPoint().getCol() - 1);
+                    if (playerPoint.getCol() > 0 && points[playerPoint.getRow()][playerPoint.getCol() - 1].isOccupied())
+                        player.getPoint().setCol(player.getPoint().getCol() - 1);
                     break;
                 case 4:
-                    player.getPoint().setCol(player.getPoint().getCol() + 1);
+                    if (playerPoint.getRow() < 28 && points[playerPoint.getRow()][playerPoint.getCol() + 1].isOccupied())
+                        player.getPoint().setCol(player.getPoint().getCol() + 1);
                     break;
             }
         }
