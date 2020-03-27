474e51d7860f25dbffad5cbdc03402098aac57cb
grzegorz103 <grzesiek555_12@outlook.com>
2020-03-27 15:34:33 +0100

    Fix rwd

--- chess2/src/app/maze/multiplayer/multiplayer.component.ts
+++ chess2/src/app/maze/multiplayer/multiplayer.component.ts
@@ -172,4 +172,18 @@ export class MultiplayerComponent implements OnInit {
     }
 
   }
+
+  copyMessage(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
 }
