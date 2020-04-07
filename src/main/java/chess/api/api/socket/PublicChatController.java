1f1d8483f13b9ae128ee543f65e7039ce5a2c2c0
grzegorz103 <grzesiek555_12@outlook.com>
2020-04-07 19:28:12 +0200

    Add MemberComparator interface

--- src/main/java/chess/api/api/socket/PublicChatController.java
+++ src/main/java/chess/api/api/socket/PublicChatController.java
@@ -1,26 +1,20 @@
 package chess.api.api.socket;
 
 import chess.api.api.utils.WebSocketUtils;
-import chess.api.domain.maze.Maze;
 import chess.api.domain.publicChat.Member;
 import chess.api.domain.publicChat.Message;
 import chess.api.services.PublicChatServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.Set;
+import java.util.List;
 
 @Controller
 public class PublicChatController {
@@ -54,7 +48,7 @@ public class PublicChatController {
     }
 
     @EventListener
-    public Set<Member> handleSessionDisconnect(SessionDisconnectEvent event) {
+    public List<Member> handleSessionDisconnect(SessionDisconnectEvent event) {
         chatService.removeMember(event.getSessionId());
         messagingTemplate.convertAndSend("/queue/public/chat/users", chatService.getMembers());
         return chatService.getMembers();
