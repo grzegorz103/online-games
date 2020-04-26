22258b2b7541116795a96e72dfedc6a11535c6c3
grzegorz103 <grzesiek555_12@outlook.com>
2020-04-26 19:09:18 +0200

    Add click events

--- front/src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ front/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tic-tac-toe',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicTacToeComponent implements OnInit {
 
-  constructor() { }
+  grid: string[] = [];
+  readonly X_POINT: string = 'X';
+  readonly Y_POINT: string = 'Y';
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.initGrid();
+  }
+
+  initGrid() {
+    for (let i: number = 0; i < 9; i++) {
+      this.grid[i] = ''
+    }
+  }
+
+  isXOnField(i: number){
+    return this.grid[i] == this.X_POINT;
+  }
+
+  isYOnField(i: number){
+    return this.grid[i] == this.Y_POINT;
+  }
+
+  move(i: number){
+    this.grid[i] = this.Y_POINT;
+  }
 }
